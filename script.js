c2f7b588d3dfb7efde77736e3d247f54b77012d5
LuizBlume <luiz.fernando.251107@gmail.com>
2023-11-11 11:02:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let valor1 = document.querySelector('#valor1');
+let valor2 = document.querySelector('#valor2');
+let resultado = document.querySelector('#resultado');
+valor1.addEventListener('keyup', function () {
+    somar(Number(valor1.value), Number(valor2.value));
+});
+valor2.addEventListener('keyup', function () {
+    somar(Number(valor1.value), Number(valor2.value));
+});
+function somar(a, b) {
+    let soma = a + b;
+    resultado.innerHTML = soma;
+}
